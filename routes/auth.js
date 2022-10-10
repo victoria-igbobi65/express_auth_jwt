febe12422cca99210aab11a394b790e4c6a278ba
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-10-10 21:14:12 +0100

    user routes

---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const authController = require('../controllers/auth')
+require('dotenv').config();
+
+const authRouter = express.Router();
+
+authRouter.post(
+    '/signup',
+    passport.authenticate('signup', { session: false }), authController.signup 
+);
+
+authRouter
+    .route('/login')
+    .post(authController.login)
+
+module.exports = authRouter;
