0429cef7ba36a803125b79e3aa04c7e7e013463a
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-10-16 22:09:24 +0100

    updated post routes

--- routes/post.js
+++ routes/post.js
@@ -5,5 +5,12 @@ const postRouter = express.Router()
 postRouter
     .route('/')
     .post(postController.createPost)
+    .get(postController.getAllPost)
+
+postRouter
+    .route('/:id')
+    .get(postController.getPostById)
+    .patch(postController.updatePost)
+    .delete(postController.deletePostById)
 
 module.exports=postRouter
