2ebab0ae7afed9a5ce571bb689e292f62bd64a49
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-10-16 18:12:56 +0100

    added some user controllers

--- controllers/user.js
+++ controllers/user.js
@@ -12,3 +12,58 @@ exports.getAllUser = async(req, res, next) =>{
             users
         })
 }
+
+
+exports.getUserById = async (req, res, next) =>{
+    const {id} = req.params
+    try{
+
+        const user = await User
+                            .findById(id)
+                            .populate('posts')
+
+        return res
+                .status(200)
+                .json({
+                    status: true,
+                    user
+                })
+    }
+    catch(err){
+        return next(`This error occured while handling that \n${err}`)
+    }
+}
+
+
+exports.deleteUserById = async(req, res, next) =>{
+    const {id} = req.params
+
+    try{
+        const user = await User.findById(id)
+        if (!user){
+            return res
+                    .status(404)
+                    .json({
+                        status: false,
+                        msg: "user with id doesn't exist!"
+                    })
+        }
+
+        await User.deleteOne({id: id})
+
+        return res
+                .status(200)
+                .json({
+                    status: true,
+                    msg: null
+                })
+   }
+    catch(err){
+        res
+            .status(400)
+            .json({
+                status: false,
+                err
+            })
+    }
+}
