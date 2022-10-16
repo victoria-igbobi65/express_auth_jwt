209dffb368892eb514c91ae27a552824ad565d8f
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-10-16 22:08:50 +0100

    completed post controllers

--- controllers/post.js
+++ controllers/post.js
@@ -5,52 +5,59 @@ const moment = require('moment')
 
 
 exports.createPost = async (req, res, next) =>{
-
-
-    // FIND THE ID OF USER CREATING NEW POST
-    const user = await User.findById(req.user._id)
-
-    // SAVE NEW POST TO DB
-    const newPost = await Post.create({
-        post: req.body.post,
-        posted_at: moment().toDate()
-    })
-
-
-    //SAVE THE ID OF THE NEW POST TO THE ARRAY OF POSTS BELONGING TO THE USER IN THE USER MODEL
-    user.posts = user.posts.concat(newPost._id)
-    await user.save()
-    
-    res
-        .status(200)
-        .json({
-            newPost
-        })
-}
-
-exports.getPostById = async (req, res, next) => {
-
-    const {id} = req.params
     try{
-        const post = await Post.find({id: id})
+        // GET USER ID
+        const id = req.user._id
 
+        // FIND USER FROM THE DB
+        const user = await User.findById(id)
+        
+        const newPost = await Post.create({
+            post: req.body.post,
+            posted_at: moment().toDate()
+        })
+
+        // SAVE THE POST ID TO THE USER POSTS LIST
+        user.posts = user.posts.concat(newPost._id)
+        await user.save()
+        
         return res
                 .status(200)
                 .json({
                     status: true,
-                    post
+                    newPost
                 })
     }
     catch(err){
         return res
                 .status(400)
                 .json({
-                    status: false,
                     err
                 })
     }
-
 }
+
+
+exports.getPostById = async (req, res, next) => {
+
+    try{
+        const { id } = req.params;
+        const post = await Post.findById(id);
+        return res
+                .status(200)
+                .json({
+                    post,
+                });
+    }
+    catch(err){
+        return res
+                .status(400)
+                .json({
+                    err,
+        });
+    }
+}
+
 
 exports.getAllPost = async (req, res, next) => {
     try{
@@ -114,5 +121,25 @@ exports.updatePost = async (req, res, next) => {
                     status: false,
                     err,
                 });
+    }
+}
+
+exports.deletePostById = async(req, res, next) => {
+    const {id} = req.params
+    try{
+        const post = await Post.deleteOne({id: id})
+
+        return res
+                .status(200)
+                .json({
+                    status: true,
+                    msg: null
+                })
+    }
+    catch(err){
+        return res.status(400).json({
+          status: false,
+          err,
+        });
     }
 }
