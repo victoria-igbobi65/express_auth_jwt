179b3c26ba3156abcf5507a70ec2334e347b47f5
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-10-16 18:12:17 +0100

    added post controllers

--- controllers/post.js
+++ controllers/post.js
@@ -15,7 +15,7 @@ exports.createPost = async (req, res, next) =>{
         post: req.body.post,
         posted_at: moment().toDate()
     })
-    
+
 
     //SAVE THE ID OF THE NEW POST TO THE ARRAY OF POSTS BELONGING TO THE USER IN THE USER MODEL
     user.posts = user.posts.concat(newPost._id)
@@ -28,6 +28,91 @@ exports.createPost = async (req, res, next) =>{
         })
 }
 
-exports.getPostById = (req, res, next) => {
+exports.getPostById = async (req, res, next) => {
 
+    const {id} = req.params
+    try{
+        const post = await Post.find({id: id})
+
+        return res
+                .status(200)
+                .json({
+                    status: true,
+                    post
+                })
+    }
+    catch(err){
+        return res
+                .status(400)
+                .json({
+                    status: false,
+                    err
+                })
+    }
+
+}
+
+exports.getAllPost = async (req, res, next) => {
+    try{
+
+        const posts = await Post
+                                .find()
+                                .populate('users')
+        
+
+        return res
+                .status(200)
+                .json({
+                    status: true,
+                    posts
+                })
+    }
+    catch(err){
+        return res
+                .status(400)
+                .json({
+                    status: false,
+                    err,
+                });
+    }
+}
+
+
+exports.updatePost = async (req, res, next) => {
+    const {id} = req.params
+    const newPost = req.body
+
+    try{
+        // FIND POST USINNG ID
+        const post = await Post
+                            .find({id: id})
+                            .populate('users')
+
+        if (!post){
+            return res
+                    .status(404)
+                    .json({
+                        status: false,
+                        msg: `post with ID ${id} doesn't exist!`
+                    })
+        }
+
+        post.post = newPost
+        await post.save()
+
+        return res
+                .status(200)
+                .json({
+                    status: true,
+                    post
+                })
+    }
+    catch(err){
+        return res
+                .status(400)
+                .json({
+                    status: false,
+                    err,
+                });
+    }
 }
